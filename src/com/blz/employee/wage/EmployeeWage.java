5f305e42075c1ef5e60026b87566f8e3c195dc9c
VijaykumarBhavanur <vijaykumarbhavanur@gmail.com>
2022-09-03 21:19:50 +0530

    Added: Employee Wage using switch caselogic

--- src/com/blz/employee/wage/EmployeeWage.java
+++ src/com/blz/employee/wage/EmployeeWage.java
@@ -7,25 +7,30 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-		int wagePerHour=20;
-		int fullDayHours=8;
-		int halfDayHours=4;
-		int salary=0;
-		
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int halfDayHours = 4;
+		int salary = 0;
+
 		Random random = new Random();
 		int randomNum = random.nextInt(3);
 
-		if (randomNum == 1)
-		{
+		switch (randomNum) {
+		case 0:
+			System.out.println("Employee is Absent");
+			break;
+
+		case 1:
 			System.out.println("Employee is Present");
 			salary = wagePerHour * fullDayHours;
-		}
-		else if(randomNum == 2)
+			break;
+
+		case 2:
 			salary = wagePerHour * halfDayHours;
-		else
-			System.out.println("Employee is Absent");
-		
-		System.out.println("Employee salary is : "+salary);
+			break;
+		}
+
+		System.out.println("Employee salary is : " + salary);
 
 	}
 
